9fa68c7514fbcbebdafd1f01d7f7483ab2498de4
SiphoChris <siphonkebe@gmail.com>
2024-09-02 14:14:51 +0200

    installing necessary dependencies

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 
 const app = createApp(App)
 app.use(store)
