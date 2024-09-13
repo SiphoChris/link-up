914294fcff8c79bbdaa148f8e6cef99920784402
Sipho Nkebe <116745605+SiphoChris@users.noreply.github.com>
2024-09-13 13:49:50 +0200

    Update index.js
    cors resolver

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middleware
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next());
 server.use(corsResolver);
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
